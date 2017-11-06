b2a15b7b16f0a0fd89e963601cde4908874dec8a
Haihua Hu <jared.hu@nxp.com>
2017-11-06 15:05:47 +0800

    fbmemory: need unmap if mapping flags are not subset of previous
    upstream status: Pending
    https://bugzilla.gnome.org/show_bug.cgi?id=789952
    
    (cherry picked from commit e8723f5fbe30d481f63d7c8500de0afdcdc25d21)

--- gst-libs/gst/allocators/gstfdmemory.c
+++ gst-libs/gst/allocators/gstfdmemory.c
@@ -97,15 +97,20 @@ gst_fd_mem_map (GstMemory * gmem, gsize maxsize, GstMapFlags flags)
     if ((mem->mmapping_flags & prot) == prot) {
       ret = mem->data;
       mem->mmap_count++;
+      goto out;
     } else if ((mem->flags & GST_FD_MEMORY_FLAG_KEEP_MAPPED)
         && mem->mmap_count == 0
         && mprotect (mem->data, gmem->maxsize, prot) == 0) {
       ret = mem->data;
       mem->mmapping_flags = prot;
       mem->mmap_count++;
+      goto out;
+    } else {
+      /* if mapping flags is not a subset, need unmap first */
+      munmap ((void *) mem->data, gmem->maxsize);
+      mem->data = NULL;
+      mem->mmap_count = 0;
     }
-
-    goto out;
   }
 
   if (mem->fd != -1) {
