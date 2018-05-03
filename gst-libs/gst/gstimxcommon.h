46b347ebaa17cf8e6acd40afa7208f2141aa5a9d
Lyon Wang <lyon.wang@nxp.com>
2018-05-03 17:28:02 +0800

    gstimxcommon: add some platform marcos
    add 8mm,8mp,8mn,8ulp,imx93,imx95
    
    upstream status: imx specific
    
    Signed-off-by: Haihua Hu <jared.hu@nxp.com>

--- gst-libs/gst/gstimxcommon.h
+++ gst-libs/gst/gstimxcommon.h
@@ -73,6 +73,12 @@ typedef enum
   CC_MX8QM = CHIPCODE ('M', 'X', '8', '1'),
   CC_MX8QXP = CHIPCODE ('M', 'X', '8', '3'),
   CC_MX8M = CHIPCODE ('M', 'X', '8', '2'),
+  CC_MX8MM = CHIPCODE ('M', 'X', '8', '4'),
+  CC_MX8MN = CHIPCODE ('M', 'X', '8', '5'),
+  CC_MX8MP = CHIPCODE ('M', 'X', '8', '6'),
+  CC_MX8ULP = CHIPCODE ('M', 'X', '8', 'U'),
+  CC_MX93 = CHIPCODE ('M', 'X', '9', '3'),
+  CC_MX95 = CHIPCODE ('M', 'X', '9', '5'),
   CC_UNKN = CHIPCODE ('U', 'N', 'K', 'N')
 
 } CHIP_CODE;
@@ -117,8 +123,17 @@ typedef enum {
 #define HAS_DPU() check_feature(imx_chip_code(), DPU)
 #define HAS_DCSS() check_feature(imx_chip_code(), DCSS)
 
-#define IS_HANTRO() (CC_MX8M == imx_chip_code())
-#define IS_AMPHION() (CC_MX8QXP == imx_chip_code())
+#define IS_HANTRO() ((CC_MX8M == imx_chip_code()) || (CC_MX8MM == imx_chip_code()) || (CC_MX8MP == imx_chip_code()) )
+#define IS_AMPHION() ((CC_MX8QM == imx_chip_code()) || (CC_MX8QXP == imx_chip_code()))
+#define IS_IMX8MM() (CC_MX8MM == imx_chip_code())
+#define IS_IMX8MN() (CC_MX8MN == imx_chip_code())
+#define IS_IMX8MQ() (CC_MX8M == imx_chip_code())
+#define IS_IMX8MP() (CC_MX8MP == imx_chip_code())
+#define IS_IMX8ULP() (CC_MX8ULP == imx_chip_code())
+#define IS_IMX93() (CC_MX93 == imx_chip_code())
+#define IS_IMX95() (CC_MX95 == imx_chip_code())
+#define IS_IMX8Q() ((CC_MX8QM == imx_chip_code()) || (CC_MX8QXP == imx_chip_code()))
+#define IS_IMX6Q() (CC_MX6Q == imx_chip_code())
 
 
 /* define rotate and flip glib enum for overlaysink and imxv4l2sink */
@@ -227,6 +242,12 @@ static SOC_INFO soc_info[] = {
   {CC_MX8QM, "i.MX8QM"},
   {CC_MX8QXP, "i.MX8QXP"},
   {CC_MX8M, "i.MX8MQ"},
+  {CC_MX8MM, "i.MX8MM"},
+  {CC_MX8MN, "i.MX8MN"},
+  {CC_MX8MP, "i.MX8MP"},
+  {CC_MX8ULP, "i.MX8ULP"},
+  {CC_MX93, "i.MX93"},
+  {CC_MX95, "i.MX95"},
 };
 
 static CHIP_CODE getChipCodeFromSocid (void)
@@ -308,9 +329,15 @@ static IMXV4l2FeatureMap g_imxv4l2feature_maps[] = {
   {CC_MX7D, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE},
   {CC_MX7ULP, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE},
   {CC_MX8, TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE},
-  {CC_MX8QM, TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE},
+  {CC_MX8QM, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, FALSE},
   {CC_MX8QXP, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, FALSE},
   {CC_MX8M, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, TRUE},
+  {CC_MX8MM, TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE},
+  {CC_MX8MP, TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE},
+  {CC_MX8MN, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE},
+  {CC_MX8ULP, TRUE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE},
+  {CC_MX93, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE},
+  {CC_MX95, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, FALSE},
 };
 
 
