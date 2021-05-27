4d0313abcdcbaed50d21f8afde170c7e652d26e3
Haihua Hu <jared.hu@nxp.com>
2021-05-27 15:18:47 +0800

    MMFMWK-8967 gstimxcommom: enable gstreamer on 8ulp
    Add support for 8ulp
    
    (cherry picked from commit a6ea17d581256fa0fa9da0c16478f4980d971e06)

--- gst-libs/gst/gstimxcommon.h
+++ gst-libs/gst/gstimxcommon.h
@@ -76,6 +76,7 @@ typedef enum
   CC_MX8MM = CHIPCODE ('M', 'X', '8', '4'),
   CC_MX8MN = CHIPCODE ('M', 'X', '8', '5'),
   CC_MX8MP = CHIPCODE ('M', 'X', '8', '6'),
+  CC_MX8ULP = CHIPCODE ('M', 'X', '8', 'U'),
   CC_UNKN = CHIPCODE ('U', 'N', 'K', 'N')
 
 } CHIP_CODE;
@@ -126,6 +127,7 @@ typedef enum {
 #define IS_IMX8MN() (CC_MX8MN == imx_chip_code())
 #define IS_IMX8MQ() (CC_MX8M == imx_chip_code())
 #define IS_IMX8MP() (CC_MX8MP == imx_chip_code())
+#define IS_IMX8ULP() (CC_MX8ULP == imx_chip_code())
 #define IS_IMX8Q() ((CC_MX8QM == imx_chip_code()) || (CC_MX8QXP == imx_chip_code()))
 #define IS_IMX6Q() (CC_MX6Q == imx_chip_code())
 
@@ -239,6 +241,7 @@ static SOC_INFO soc_info[] = {
   {CC_MX8MM, "i.MX8MM"},
   {CC_MX8MN, "i.MX8MN"},
   {CC_MX8MP, "i.MX8MP"},
+  {CC_MX8ULP, "i.MX8ULP"},
 };
 
 static CHIP_CODE getChipCodeFromSocid (void)
@@ -326,6 +329,7 @@ static IMXV4l2FeatureMap g_imxv4l2feature_maps[] = {
   {CC_MX8MM, TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE},
   {CC_MX8MP, TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE},
   {CC_MX8MN, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE},
+  {CC_MX8ULP, TRUE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE},
 };
 
 
