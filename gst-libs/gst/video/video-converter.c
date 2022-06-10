5345737cbaf856e52119ff94a0659317b3afb260
Ming Qian <ming.qian@nxp.com>
2022-06-10 16:15:41 +0800

    MMFMWK-9186-3 video: Add support for BGR_12LE
    BGR_12LE is reversed RGB format with 12 bits per component,
    expanded to 16bits.
    Data in the 12 high bits, zeros in the 4 low bits,
    arranged in little endian order.
    
    Conflicts:
    	gst-libs/gst/video/video-format.h

--- gst-libs/gst/video/video-converter.c
+++ gst-libs/gst/video/video-converter.c
@@ -7329,6 +7329,7 @@ get_scale_format (GstVideoFormat format, gint plane)
     case GST_VIDEO_FORMAT_GBRA_12LE:
     case GST_VIDEO_FORMAT_GBR_16BE:
     case GST_VIDEO_FORMAT_GBR_16LE:
+    case GST_VIDEO_FORMAT_BGR_12LE:
     case GST_VIDEO_FORMAT_NV12_64Z32:
     case GST_VIDEO_FORMAT_NV12_4L4:
     case GST_VIDEO_FORMAT_NV12_32L32:
--- gst-libs/gst/video/video-info.c
+++ gst-libs/gst/video/video-info.c
@@ -859,6 +859,7 @@ fill_planes (GstVideoInfo * info, gsize plane_size[GST_VIDEO_MAX_PLANES])
       info->offset[0] = 0;
       info->size = info->stride[0] * height;
       break;
+    case GST_VIDEO_FORMAT_BGR_12LE:
     case GST_VIDEO_FORMAT_Y312_LE:
       info->stride[0] = width * 6;
       info->offset[0] = 0;
